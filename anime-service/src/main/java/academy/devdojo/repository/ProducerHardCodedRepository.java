40c850d9ba4ff2f402d21f21336ca98f229ab91c
kelsen <kelsen.brito@gmail.com>
2026-02-15 17:03:24 -0300

    32-@bean-@configuration

--- anime-service/src/main/java/academy/devdojo/repository/ProducerHardCodedRepository.java
+++ anime-service/src/main/java/academy/devdojo/repository/ProducerHardCodedRepository.java
@@ -1,6 +1,10 @@
 package academy.devdojo.repository;
 
 import academy.devdojo.domain.Producer;
+import external.dependency.Connection;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
@@ -9,8 +13,14 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
+@RequiredArgsConstructor
+@Log4j2
 public class ProducerHardCodedRepository {
     private static final List<Producer> PRODUCERS = new ArrayList<>();
+
+    //private final Connection connectionMySql;
+    //@Qualifier(value = "connectionMongoDB")
+    private final Connection connection;
 
     static {
         PRODUCERS.addAll(
@@ -23,6 +33,7 @@ public class ProducerHardCodedRepository {
     }
 
     public List<Producer> findAll(){
+        log.debug(connection);
         return PRODUCERS;
     }
 
---
+++ anime-service/src/main/java/external/dependency/Connection.java
@@ -0,0 +1,12 @@
+package external.dependency;
+
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@ToString
+public class Connection {
+    private String host;
+    private String username;
+    private String password;
+}
