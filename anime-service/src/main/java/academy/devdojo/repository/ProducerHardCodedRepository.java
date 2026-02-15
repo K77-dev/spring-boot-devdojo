8d9d684fd09f37be993a56412466272dc392f540
kelsen <kelsen.brito@gmail.com>
2026-02-15 17:43:33 -0300

    32-testes-unitarios-01

--- anime-service/src/main/java/academy/devdojo/repository/ProducerHardCodedRepository.java
+++ anime-service/src/main/java/academy/devdojo/repository/ProducerHardCodedRepository.java
@@ -4,11 +4,8 @@ import academy.devdojo.domain.Producer;
 import external.dependency.Connection;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -16,46 +13,39 @@ import java.util.Optional;
 @RequiredArgsConstructor
 @Log4j2
 public class ProducerHardCodedRepository {
-    private static final List<Producer> PRODUCERS = new ArrayList<>();
+    
+    private final ProducerData producerData;
 
     //private final Connection connectionMySql;
     //@Qualifier(value = "connectionMongoDB")
     private final Connection connection;
 
-    static {
-        PRODUCERS.addAll(
-                List.of(
-                        Producer.builder().id(1L).name("Mappa").createdAt(LocalDateTime.now()).build(),
-                        Producer.builder().id(2L).name("Kyoto Animation").createdAt(LocalDateTime.now()).build(),
-                        Producer.builder().id(3L).name("Madhouse").createdAt(LocalDateTime.now()).build()
-                )
-        );
-    }
+
 
     public List<Producer> findAll(){
         log.debug(connection);
-        return PRODUCERS;
+        return producerData.getProducers();
     }
 
     public Optional<Producer> findById(Long id){
-        return PRODUCERS.stream()
+        return producerData.getProducers().stream()
                 .filter( n -> n.getId().equals(id))
                 .findFirst();
     }
 
     public List<Producer> findByName(String name){
-        return PRODUCERS.stream()
+        return producerData.getProducers().stream()
                 .filter(n -> n.getName().equalsIgnoreCase(name))
                 .toList();
     }
 
     public Producer save(Producer producer){
-        PRODUCERS.add(producer);
+        producerData.getProducers().add(producer);
         return producer;
     }
 
     public void delete(Producer producer){
-        PRODUCERS.remove(producer);
+        producerData.getProducers().remove(producer);
     }
 
     public void update(Producer producer){
---
+++ anime-service/src/test/java/academy/devdojo/repository/ProducerHardCodedRepositoryTest.java
@@ -0,0 +1,47 @@
+package academy.devdojo.repository;
+
+import academy.devdojo.domain.Producer;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@ExtendWith(MockitoExtension.class)
+class ProducerHardCodedRepositoryTest  {
+
+    @InjectMocks
+    private ProducerHardCodedRepository repository;
+
+    @Mock
+    private ProducerData producerData;
+    private final List<Producer> producers = new ArrayList<>();
+
+    @BeforeEach
+    void init(){
+        producers.addAll(
+                List.of(
+                        Producer.builder().id(1L).name("Ufotable").createdAt(LocalDateTime.now()).build(),
+                        Producer.builder(  ).id(2L).name("Wit Studio").createdAt(LocalDateTime.now()).build(),
+                        Producer.builder().id(3L).name("Studio Guibli ").createdAt(LocalDateTime.now()).build()
+                )
+        );
+        BDDMockito.when( producerData.getProducers()).thenReturn(producers);
+    }
+
+    @Test
+    @DisplayName("findAll return a list with all producers")
+    void findAll(){
+
+        var producers = repository.findAll();
+        Assertions.assertThat(producers).isNotNull().hasSize(producers.size() );
+    }
+}
